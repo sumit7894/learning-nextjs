ba96910e3feab3b7160353b80069c132b8a8ca44
Sumit Maurya <smithdashdash@gmail.com>
2024-02-15 15:11:40 +0530

    added langchain route

--- app/api/openai/route.js
+++ app/api/openai/route.js
@@ -1,6 +1,24 @@
+import { ChatOpenAI } from "@langchain/openai";
 import { NextResponse } from "next/server";
 
+const openai = process.env.OPENAI_API_KEY;
+
 export async function POST(req){
-    const res = await req.json();
-    return NextResponse.json({"msg":"Post Success this is a message from a openai route"})
+    const {text} = await req.json();
+    // console.log("this is text",text);
+    const chatModel = new ChatOpenAI({
+        openAIApiKey: openai,
+      });
+
+      let final_result = await chatModel.invoke(`
+      From the following data generate the 5 mcq questions with 4 options
+      and one correct answer. Return the result in json format
+      
+      ${text}
+      `)
+      const gptResponse = final_result.content;
+      console.log("this is gpt response",gptResponse);
+    return new NextResponse(gptResponse)
+
+    // return NextResponse.json("hello");   
 }
--- app/page.js
+++ app/page.js
@@ -6,24 +6,54 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
 
-  const [message,setMessage] = useState("");
+  // const [message,setMessage] = useState();
+  const [question,setQuestion] = useState([]);
+  const [alert,setAlert] = useState("");
+  const text = {text:"In summary, an API endpoint is a specific location within an API that accepts requests and sends back responses. It's a way for different systems and applications to communicate with each other, by sending and receiving information and instructions via the endpoint."}
 
-  const text = {prompt:"hey this is prompt for chatgpt"}
-
-  useEffect(()=>{
-    const getResponse = async()=>{
-      const response = await 
-      axios.post('/api/openai',text);
-      console.log("this is response",response.data.msg);
-      setMessage(response.data.msg);
+  const getResponse = async()=>{
+    setAlert("Generating Questions Please wait...")
+    const response = await 
+    axios.post('/api/openai',text);
+    console.log("this is full response",response);
+    console.log("this is data in response",response.data);
+    console.log("this is questions inside data",response);
+    setQuestion(response.data.questions);
+    if(question){
+      setAlert("Generated Hurray");
     }
+    console.log("this is question",question);
+  }
+
+  const handleQuestions =()=>{
+    
     getResponse();
-  },[])
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello nextjs
-      {message}
+     <div>
+      <button onClick={handleQuestions} 
+      style={{backgroundColor:"black",color:"white"}}
+      >
+        Genrate Questions
+      </button>
+      <h1>{alert}</h1>
+      {question?.map((item, index) => (
+        <div key={index}>
+          <p>{item?.question}</p>
+          <ul>
+            {item?.options?.map((option, optionIndex) => (
+              <li key={optionIndex}>{option}</li>
+            ))}
+          </ul>
+          <br/>
+         <h1>Question end</h1>
+         <br/>
+        </div>
+      ))}
+    </div>
+      
     </main>
   );
 }
